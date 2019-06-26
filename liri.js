bf837f180e3c2d6c03cee17689ce89650d42f3d5
max delzer <emma.delzer@gmail.com>
2019-06-26 11:15:55 -0700

    Created app JS file

---
+++ liri.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+
+//Make it so liri.js can take in one of the following commands:
+
+//concert-this
+//spotify-this-song
+//movie-this
+//do-what-it-says
