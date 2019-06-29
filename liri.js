d08e8befd6c03a7f71e2383220311d7c67ebac10
max delzer <emma.delzer@gmail.com>
2019-06-28 22:15:20 -0700

    Finished concertThis, spotifyThis, and movieThis

--- liri.js
+++ liri.js
@@ -2,97 +2,99 @@
 require("dotenv").config();
 
 // code to import keys.js and store in variable
-const keys = require("./keys.js"); 
+var keys = require("./keys.js"); 
 
+// NPM package
+var Spotify = require("node-spotify-api");
 // Initialize Spotify
-const Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 
 // Require AXIOS
-const axios = require('axios');
+var axios = require('axios');
 
 // Require Moment
-const moment = require('moment'); 
-
-// Require file systems
+var moment = require('moment');
 var fs = require('fs');
+var query = process.argv[3];
 
 // Take User command and input
-const [, , command, ...searchInput] = process.argv[2];
+var option = process.argv[2];
 
-// switchboard for commands
-switch (command){
-    case "concert-this":
-        concertThis();
-        break;
-    case "spotify-this-song":
-        spotifyThis();
-        break;
-    case "movie-this":
-        movieThis();
-        break;
-    case "do-what-it-says":
-        doThis(userQuery);
-        break;
+
+switch (option) {
+  case "movie-this":
+      movieThis(query);
+      break;
+  case "spotify-this-song":
+      spotifyCall(query);
+      break;
+  case "concert-this":
+      concertThis(query);
+      break;
+  default:
+      // 1- read file
+      fs.readFile("random.txt", "utf8", function (error, data) {
+          // 2-retrieve content & parse string
+          var data = data.split(",");
+          var thatWay = data[1];
+          if (error) {
+              return console.log(error);
+          }
+          // 3-call function 
+          spotifyCall(thatWay);
+      })
+
 }
-//==========================================
-//Make it so liri.js can take in one of the following commands:
+
+// FUNCTIONS
 
 //concert-this
-function concertThis() {
+function concertThis(artist) {
   //command syntax 'node liri.js concert-this <artist/band name here>''
-  console.log("\n------\n\nSearching for..${userQuery}'s next show...");
-  searchItem = searchInput.join('')
-    // console.log(searchItem)
-    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-        .then(artist => {
-            for (let i = 0; i < artist.data.length; i++) {
-                let concertDateTime = artist.data[i].datetime
-                let concertDate = concertDateTime.slice(0, 10)
-                console.log(`
-                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-                Result ${i+1} of ${artist.data.length}
-                Name of Venue: ${artist.data[i].venue.name}
-                City: ${artist.data[i].venue.city}
-                Date: ${concertDate}
-                `)
-            }
-        })
-};
+  var bandsQueryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+  axios.get(bandsQueryUrl).then(
+    function (response) {
+        console.log("_Upcoming Events_");
+        console.log("Artist: " + artist + "\nVenue: " + response.data[0].venue.name + "\nLocation: " + response.data[0].venue.country + "\nDate: " + response.data[0].datatime + "\nExcellent taste, User!");
+    });
+}
 
 //spotify-this-song
   // syntax 'node liri.js spotify-this-song '<song name here>''
-function spotifyThis() {
-  searchItem = searchInput.join(' ')
-  console.log(searchItem)
-  spotify.search({ type: 'track', query: `${searchItem}` }, function(err, data) {
-      if (err) {
-        return console.log('Error occurred: ' + err);
-      }
-      for (let i = 0; i < data.tracks.items[0].artists.length; i++) {
-      console.log(`
-      ~~~~~~~~~~
-      Artist(s): ${data.tracks.items[0].artists[i].name}`)
-      }
-      console.log(`
-      Song Name: ${data.tracks.items[0].name}
-      Preview: ${data.tracks.items[0].preview_url}
-      Album: ${data.tracks.items[0].album.name}
-      ~~~~~~~~~~
-      `)
-  //   console.log(data); 
+  function spotifyCall(songName) {
+    spotify.search({ type: 'track', query: songName }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log("\n_Track Info_" + "\nArtist: " + data.tracks.items[0].artists[0].name + "\nSong: " + data.tracks.items[0].name + "\nLink: " + data.tracks.items[0].external_urls.spotify + "\nAlbum: " + data.tracks.items[0].album.name + "\n" + "\nLIRI loves this track!")
     });
-};
+}
 
 //movie-this
   // syntax 'node liri.js movie-this '<movie name here>''
-function movieThis() {
+function movieThis(movieName) {
+    if (!movieName) {
+        movieName = "Mr. Nobody";
+    }
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    // Creating a request with axios to the queryUrl
+    axios.get(queryUrl).then(
+        function (response) {
+            if (!movieName) {
+                movieName = "Mr. Nobody";
+            }// console.log(response.data);
+            // Data of Movie
+            console.log("\n_Movie Info_" + "\nTitle: " + response.data.Title + "\nRelease Year: " + response.data.Year + "\nRating: " + response.data.Rated + "\nRelease Country: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nActors: " + response.data.Actors + "\n" + "\nLIRI Query: Has user seen this yet?");
 
-  };
+
+        }
+    );
+}
 
 //do-what-it-says
   // syntax 'node liri.js do-what-it-says'
-function doThis(){
+function doWhatItSays(){
 
-};
+}
   
