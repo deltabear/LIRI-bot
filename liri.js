d6c2208fe3f40f7b37422e6b2008bff30c416344
max delzer <emma.delzer@gmail.com>
2019-06-28 20:15:24 -0700

    Made switch consistent, added axios request

--- liri.js
+++ liri.js
@@ -18,40 +18,70 @@ const moment = require('moment');
 var fs = require('fs');
 
 // Take User command and input
-let userInput = process.argv[2];
-let userQuery = process.argv.slice(3).join(" ");
+const [, , command, ...searchInput] = process.argv[2];
 
-function userCommand(userInput, userQuery) {
-  // switchboard for commands
-  switch (command){
+// switchboard for commands
+switch (command){
     case "concert-this":
         concertThis();
         break;
-    case "spotify-this":
+    case "spotify-this-song":
         spotifyThis();
         break;
     case "movie-this":
         movieThis();
         break;
-    case "do-this":
+    case "do-what-it-says":
         doThis(userQuery);
         break;
-  }
 }
-userCommand(userInput, userQuery);
+//==========================================
 //Make it so liri.js can take in one of the following commands:
 
 //concert-this
 function concertThis() {
   //command syntax 'node liri.js concert-this <artist/band name here>''
-  console.log(`\n------\n\nSEARCHING FOR...${userQuery}'s next show...`);
-
+  console.log("\n------\n\nSearching for..${userQuery}'s next show...");
+  searchItem = searchInput.join('')
+    // console.log(searchItem)
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+        .then(artist => {
+            for (let i = 0; i < artist.data.length; i++) {
+                let concertDateTime = artist.data[i].datetime
+                let concertDate = concertDateTime.slice(0, 10)
+                console.log(`
+                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                Result ${i+1} of ${artist.data.length}
+                Name of Venue: ${artist.data[i].venue.name}
+                City: ${artist.data[i].venue.city}
+                Date: ${concertDate}
+                `)
+            }
+        })
 };
 
 //spotify-this-song
   // syntax 'node liri.js spotify-this-song '<song name here>''
 function spotifyThis() {
-
+  searchItem = searchInput.join(' ')
+  console.log(searchItem)
+  spotify.search({ type: 'track', query: `${searchItem}` }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+      for (let i = 0; i < data.tracks.items[0].artists.length; i++) {
+      console.log(`
+      ~~~~~~~~~~
+      Artist(s): ${data.tracks.items[0].artists[i].name}`)
+      }
+      console.log(`
+      Song Name: ${data.tracks.items[0].name}
+      Preview: ${data.tracks.items[0].preview_url}
+      Album: ${data.tracks.items[0].album.name}
+      ~~~~~~~~~~
+      `)
+  //   console.log(data); 
+    });
 };
 
 //movie-this
